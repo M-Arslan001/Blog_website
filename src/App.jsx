2e9d839c75bda8b872ea0ab7483a7c22223eda5f
M-Arslan011 <arslanashraf185@gmail.com>
2024-07-20 20:30:32 +0500

    new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+// import authSlice from "./store/authSlice";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <h1>Blog app in appwrite using the reactjs</h1>
-    </>
-  );
+  const [loading, seteLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => seteLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className=" w-full block">
+        <Header />
+        <main>TODO: {/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
